eddb23046267f69febf5f37bb92f168647867eeb
Ed Kellett <edk141@gmail.com>
2013-01-28 15:34:38 +0000

    add initial plugins (ban manager does nothing)

---
+++ ZomgBot/plugins/ban_manager.py
@@ -0,0 +1,12 @@
+from ZomgBot.plugins import Plugin, Modifier
+from ZomgBot.events import EventHandler
+
+@Plugin.register(depends=None, provides=None)
+class BanManager(Plugin):
+    @Modifier.command("ban", permission="channel.admin")
+    def ban(self, context, target):
+        print "Ban {}".format(target)
+
+    @EventHandler("IJoinChannel")
+    def test(self, event):
+        print "I am joining channel {}".format(event)
---
+++ ZomgBot/plugins/commands.py
@@ -0,0 +1,45 @@
+from ZomgBot.plugins import Plugin, Modifier
+from ZomgBot.events import EventHandler
+
+class CommandContext(object):
+    def __init__(self, user, channel):
+        self.user = user
+        self.channel = channel
+
+    def reply(self, msg, public=True):
+        if self.channel and public:
+            self.channel.say(msg)
+        else:
+            self.user.say(msg)
+    
+    def parse_args(self, msg):
+        self.args = msg[1:].split(' ')
+        return self.args.pop(0)
+
+@Plugin.register(depends=None, provides=None)
+class Commands(Plugin):
+    @EventHandler("PluginsLoaded")
+    def handle_reload(self, event):
+        cmds = Modifier.get("command")
+        self.commands = {}
+        for cmd in cmds:
+            [self.commands.update({n: cmd}) for n in cmd.annotation["command"].get("aliases", []) + [cmd.annotation["command"]["args"][0]]]
+        print self.commands
+
+    def do_command(self, name, context):
+        print "Trying to execute {}".format(name)
+        if name in self.commands:
+            self.commands[name].call_with_self(context)
+        else:
+            context.reply("No such command, try another one you retard.")
+
+    @EventHandler("ChannelMsg")
+    def handle_commands(self, event):
+        if not event.message.startswith('/'): return
+        context = CommandContext(event.user, event.channel)
+        command = context.parse_args(event.message)
+        self.do_command(command, context)
+
+    @Modifier.command("derp")
+    def cmd_derp(self, context):
+        context.reply("hello, {}!".format(context.user))
