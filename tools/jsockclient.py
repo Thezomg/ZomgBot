ffdfb2da7a74ee49bcd94e7cbdd5cc6f523bf1a0
Deaygo <deaygo@thezomg.com>
2013-01-30 13:58:59 -0500

    Added starting for jsock to trigger events

--- tools/jsockclient.py
+++ tools/jsockclient.py
@@ -3,6 +3,6 @@ from time import sleep
 import sys
 client = jsocket.JsonClient(port=5489)
 client.connect()
-client.send_obj({"function": "say", "args": [sys.argv[1]]})
+client.send_obj({"function": "Testing", "args": [sys.argv[1]]})
 sleep(1)
 client.close()
